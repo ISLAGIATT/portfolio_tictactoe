b7d0abb2d0fbf122bc22bfc9aee0e9f601fc4d94
ISLAGIATT <133043444+ISLAGIATT@users.noreply.github.com>
2023-12-12 12:46:47 -0800

    improved error handling for occupied space and simplified loops

--- main.py
+++ main.py
@@ -11,39 +11,41 @@ def x_move():
             x_axis = int(input("Player X, please enter your desired row (1-3): ")) - 1
             if x_axis < 0 or x_axis > 2:
                 raise ValueError("Number outside of range. Pick 1-3")
-            break
-        except ValueError as e:
-            print(e)
 
-    while True:
-        try:
             y_axis = int(input("Player X, please enter your desired column (1-3): ")) - 1
             if y_axis < 0 or y_axis > 2:
                 raise ValueError("Number outside of range. Pick 1-3")
-            break
+
+            if gameboard[x_axis][y_axis] == "*":
+                break
+            else:
+                print("Space already taken. Please try again.")
         except ValueError as e:
             print(e)
 
     gameboard[x_axis][y_axis] = 'X'
+
+
 def o_move():
     while True:
         try:
             x_axis = int(input("Player O, please enter your desired row (1-3): ")) - 1
             if x_axis < 0 or x_axis > 2:
                 raise ValueError("Number outside of range. Pick 1-3")
-            break
-        except ValueError as e:
-            print(e)
 
-    while True:
-        try:
             y_axis = int(input("Player O, please enter your desired column (1-3): ")) - 1
             if y_axis < 0 or y_axis > 2:
                 raise ValueError("Number outside of range. Pick 1-3")
-            break
+
+            if gameboard[x_axis][y_axis] == "*":
+                break
+            else:
+                print("Space already taken. Please try again.")
         except ValueError as e:
             print(e)
+
     gameboard[x_axis][y_axis] = 'O'
+
 def check_win_conditions(player):
     win_patterns = [
         [(0, 0), (0, 1), (0, 2)],  # Top row
@@ -66,6 +68,8 @@ while game_on:
         print(i)
     x_move()
     if check_win_conditions("X"):
+        for i in gameboard:
+            print(i)
         print("X Player wins!")
         game_on = False
         break
@@ -73,6 +77,8 @@ while game_on:
         print(i)
     o_move()
     if check_win_conditions("O"):
+        for i in gameboard:
+            print(i)
         print("O Player Wins!")
         game_on = False
         break
